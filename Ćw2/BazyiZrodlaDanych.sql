4607f5f2283097a3b055075e6eecdbfe1214ac12
KryPote <krystian.potepa@gmail.com>
2021-01-10 23:27:48 +0100

    almost everything in 2nd execirse

--- Ćw2/BazyiZrodlaDanych.sql
+++ Ćw2/BazyiZrodlaDanych.sql
@@ -1,8 +1,4 @@
-CREATE VIEW CZTERY AS SELECT FIRST_NAME, LAST_NAME, round(months_between(current_date, HIRE_DATE)) "LICZBA_MIESIECY",
-    case 
-    when round(months_between(current_date, HIRE_DATE)) <= 149 THEN SALARY * 0.1
-    when round(months_between(current_date, HIRE_DATE)) between 150 and 200 THEN SALARY * 0.2
-    when round(months_between(current_date, HIRE_DATE)) >= 200 THEN SALARY * 0.3
-    else 0 end as WYSOKOSC_DODATKU
-    from EMPLOYEES
-    order by "LICZBA_MIESIECY";
+CREATE VIEW SIEDEM AS SELECT EMPLOYEES.FIRST_NAME, EMPLOYEES.LAST_NAME FROM EMPLOYEES WHERE DEPARTMENT_ID in 
+    (
+    SELECT DEPARTMENT_ID FROM EMPLOYEES WHERE FIRST_NAME = 'Jennifer'
+    );
