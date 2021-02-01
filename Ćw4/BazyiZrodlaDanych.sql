819b4a6c4db51d8b7804dece86598136c5974cad
KryPote <krystian.potepa@gmail.com>
2021-02-01 17:06:45 +0100

    cw5

--- Ćw4/BazyiZrodlaDanych.sql
+++ Ćw4/BazyiZrodlaDanych.sql
@@ -1,15 +1,1 @@
-declare 
-    cursor Seven is select 'SALARY', 'LAST_NAME' from EMPLOYEES where DEPARTMENT_ID = 50;
-    
-begin
-for cursor_row in Seven
-    loop
-        if cursor_row.SALARY > 3100 
-	then
-            dbms_output.put_line(cursor_row.LAST_NAME || ' nie dawaæ podwy¿ki');
-        else
-            dbms_output.put_line(cursor_row.LAST_NAME ||' daæ podwy¿kê');
-        end if;
-    end loop;
-close Seven;
-end;
+CREATE TABLE archiwum_departamentów(dep_id int primary key, dep_name varchar, close_date date, last_manager varchar);
